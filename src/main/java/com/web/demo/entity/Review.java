333421f54074fcdf3fc5b8597e6b3d32e8c3c06b
ucoccto <edu1.ucoc@gmail.com>
2024-08-23 14:43:43 +0900

    Feat: rest api, swagger part 1

--- src/main/java/com/web/demo/entity/Review.java
+++ src/main/java/com/web/demo/entity/Review.java
@@ -2,12 +2,16 @@ package com.web.demo.entity;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import groovy.transform.ToString;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Getter;
